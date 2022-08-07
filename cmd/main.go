69bca52edac5657a3cce06b6c678325abacf4e94
Dimitar Kostov <dimitarkostov@Dimitars-MacBook-Pro.local>
2022-08-07 16:29:32 +0300

    feature/setup docker initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	_ "github.com/gorilla/mux"
 )
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +16,7 @@ func handleHttp() {
 	router := mux.NewRouter()
 	router.HandleFunc("/get", handleGet).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe(":8083", router))
+	log.Fatal(http.ListenAndServe(":80", router))
 }
 
 func main() {
