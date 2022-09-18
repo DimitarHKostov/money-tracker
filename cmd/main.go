93524409018bfbff7d2db28a43c0ce5839d245a5
Dimitar Kostov <dimitarkostov@Dimitars-MacBook-Pro.local>
2022-09-18 18:26:46 +0300

    add db credentials formatter

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"money-tracker/pkg/app"
 	"money-tracker/pkg/cors"
-	"money-tracker/pkg/database/database_type"
+	"money-tracker/pkg/database/database_config"
 	"money-tracker/pkg/generator"
 	"money-tracker/pkg/jwt"
 
@@ -12,15 +12,34 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const (
+	apiVersion = "v1"
+	appPort    = ":80"
+	secretKey  = "asdasasdasasdasasdasasdasaa"
+	host       = "localhost"
+	port       = "5432"
+	user       = "postgres"
+	password   = "test"
+)
+
 func main() {
 	app := app.App{
-		Router: mux.NewRouter(),
+		AppRouter: mux.NewRouter(),
 		JWTManager: &jwt.JWTManager{
 			PayloadGenerator: &generator.PayloadGenerator{},
-			SecretKey:        "asdasasdasasdasasdasasdasaa",
+			SecretKey:        secretKey,
 		},
-		DatabaseType: database_type.Postgres}
-	app.Router.Use(cors.Middleware)
+		DatabaseConfig: database_config.DatabaseConfig{
+			Host:     host,
+			Port:     port,
+			Username: user,
+			Password: password,
+		},
+		AppVersion: apiVersion,
+		AppPort:    appPort,
+	}
+
+	app.AppRouter.Use(cors.Middleware)
 
 	err := app.Run()
 	if err != nil {
